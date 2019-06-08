c6f4d204b7f0157bd4c89e91a3eec2d897834c81
Agnieszka Soltysik <agnieszkasoltysik@Agnieszkas-MacBook-Air.local>
2019-06-08 13:52:23 +0200

    Add home and messages page

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import "assets/scss/material-kit-react.scss?v=1.4.0";
 // pages for this product
 import LoginPage from "views/LoginPage/LoginPage.jsx";
 import HomePage from "views/HomePage/HomePage.jsx";
+import Message from "views/Messages/Messages.jsx";
 
 var hist = createBrowserHistory();
 
@@ -15,6 +16,7 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route path="/home" component={HomePage} />
+      <Route path="/messages" component={Message} />
       <Route path="/" component={LoginPage} />
     </Switch>
   </Router>,
